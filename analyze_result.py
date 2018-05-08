05a4343dbb20863fca1683da2a24e1f349dae6ea
daksunt <davidteksenaksun@gmail.com>
2018-05-08 20:17:57 +0200

    analysis code

--- analyze_result.py
+++ analyze_result.py
@@ -1,10 +1,17 @@
 import json
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_style("whitegrid")
 
 
-def readFile():
-    file = "notebook-mttest.json"
-    with open(file) as f:
+def readFile(file):
+    exp_names = []
+    tids = []
+    ops_list = []
+
+    with open("dump/"+file) as f:
         notebook = json.load(f)
         data = notebook["data"]
 
@@ -14,7 +21,40 @@ def readFile():
 
                 ops_per_second = float(e["ops_per_sec"])
                 tid = int(e["thread"])
+                exp_name = e["test"]
+
+                tids.append(tid)
+                ops_list.append(ops_per_second)
+                exp_names.append(exp_name)
+
+    d = {
+        "tids":tids,
+        "exp_names":exp_names,
+        "ops_list":ops_list
+    }
+
+    return pd.DataFrame(d)
+
+
+def plot_df(df, file, name):
+    ax = sns.barplot(
+        x="exp_names", y="ops_list", hue="tids", data=df)
+
+    plt.title(name)
+    plt.xlabel("Thread ids")
+    plt.ylabel("Ops per second")
+    plt.savefig("dump/"+file[:-4]+"png")
 
 
 if __name__ == '__main__':
-    readFile()
+    titles = ["Global Flush", "No Global Flush"]
+
+    files = [
+        "notebook_intensive_globalflush.json",
+        "notebook_intensive_noglobalflush.json"
+    ]
+
+    for file, title in zip(files, titles):
+        df = readFile(file)
+        print(df.head())
+        plot_df(df, file, title)
