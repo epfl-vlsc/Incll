1c836a94e2793c2336ea171fc6302a3c0f538a1f
daksunt <davidteksenaksun@gmail.com>
2018-07-23 16:28:09 +0200

    better throughput

--- mttest.cc
+++ mttest.cc
@@ -584,7 +584,7 @@ static pthread_cond_t subtest_cond;
 
 
 MAKE_TESTRUNNER(rand, kvtest_rand(client, 5000000));
-MAKE_TESTRUNNER(recovery, kvtest_recovery(client));
+//MAKE_TESTRUNNER(recovery, kvtest_recovery(client));
 
 MAKE_TESTRUNNER(ycsb_a,
 kvtest_ycsb(client,
@@ -603,24 +603,42 @@ MAKE_TESTRUNNER(ycsb_c,
 		ycsbc::OpRatios(100, 0, 0, 0)));
 
 
-/*
-MAKE_TESTRUNNER(ycsb_d,
-kvtest_ycsb(client));
-*/
-
-
 MAKE_TESTRUNNER(ycsb_e,
 		kvtest_ycsb(client,
 		ycsbc::OpHelper(20000000, 1000000, 0, 20000000, ycsbc::kgd_zipfian),
 		ycsbc::OpRatios(0, 5, 0, 95)));
 
-/*
-MAKE_TESTRUNNER(ycsb_f,
-kvtest_ycsb(client));
-*/
-
 
 /*
+MAKE_TESTRUNNER(rw1, kvtest_rw1(client));
+// MAKE_TESTRUNNER(palma, kvtest_palma(client));
+// MAKE_TESTRUNNER(palmb, kvtest_palmb(client));
+MAKE_TESTRUNNER(rw1fixed, kvtest_rw1fixed(client));
+MAKE_TESTRUNNER(rw1long, kvtest_rw1long(client));
+MAKE_TESTRUNNER(rw1puts, kvtest_rw1puts(client));
+MAKE_TESTRUNNER(rw2, kvtest_rw2(client));
+MAKE_TESTRUNNER(rw2fixed, kvtest_rw2fixed(client));
+MAKE_TESTRUNNER(rw2g90, kvtest_rw2g90(client));
+MAKE_TESTRUNNER(rw2fixedg90, kvtest_rw2fixedg90(client));
+MAKE_TESTRUNNER(rw2g98, kvtest_rw2g98(client));
+MAKE_TESTRUNNER(rw2fixedg98, kvtest_rw2fixedg98(client));
+MAKE_TESTRUNNER(rw3, kvtest_rw3(client));
+MAKE_TESTRUNNER(rw4, kvtest_rw4(client));
+MAKE_TESTRUNNER(rw4fixed, kvtest_rw4fixed(client));
+MAKE_TESTRUNNER(wd1, kvtest_wd1(10000000, 1, client));
+MAKE_TESTRUNNER(wd1m1, kvtest_wd1(100000000, 1, client));
+MAKE_TESTRUNNER(wd1m2, kvtest_wd1(1000000000, 4, client));
+MAKE_TESTRUNNER(same, kvtest_same(client));
+MAKE_TESTRUNNER(rwsmall24, kvtest_rwsmall24(client));
+MAKE_TESTRUNNER(rwsep24, kvtest_rwsep24(client));
+MAKE_TESTRUNNER(wscale, kvtest_wscale(client));
+MAKE_TESTRUNNER(ruscale_init, kvtest_ruscale_init(client));
+MAKE_TESTRUNNER(rscale, if (client.ti_->index() < ::rscale_ncores) kvtest_rscale(client));
+MAKE_TESTRUNNER(uscale, kvtest_uscale(client));
+MAKE_TESTRUNNER(bdb, kvtest_bdb(client));
+MAKE_TESTRUNNER(wcol1, kvtest_wcol1at(client, client.id() % 24, kvtest_first_seed + client.id() % 48, 5000000));
+MAKE_TESTRUNNER(rcol1, kvtest_rcol1at(client, client.id() % 24, kvtest_first_seed + client.id() % 48, 5000000));
+MAKE_TESTRUNNER(wcol1o1, kvtest_wcol1at(client, (client.id() + 1) % 24, kvtest_first_seed + client.id() % 48, 5000000));
 MAKE_TESTRUNNER(intensive_small, kvtest_intensive(client, 500, 200));
 MAKE_TESTRUNNER(intensive, kvtest_intensive(client, 5000000, 2000000));
 MAKE_TESTRUNNER(rw1, kvtest_rw1(client));
