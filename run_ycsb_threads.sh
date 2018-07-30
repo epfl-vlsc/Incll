b5ba60398f3996680e8820819cab621830e1e095
daksunt <davidteksenaksun@gmail.com>
2018-07-30 10:20:16 +0200

    standard dev for sum ops

--- run_ycsb_threads.sh
+++ run_ycsb_threads.sh
@@ -13,11 +13,11 @@ echo "TotalOps,AvgOps,StdOps,Workload,Threads" >> ${Oname}
 for WORKLOAD in rand\
 	ycsb_a_uni ycsb_b_uni ycsb_c_uni ycsb_e_uni \
 	ycsb_a_zipf ycsb_b_zipf ycsb_c_zipf ycsb_e_zipf; do
-	for THREADS in 1 2 4 8; do
+	for THREADS in 1 2 4 6 8 10 12 14 16; do
 		rm -rf *.json
 		for i in $(eval echo {1..$repeat}); do 
 			rm -rf /tmp/nvm.*
-			timeout 20  ./mttest ${WORKLOAD} --nops1=1000000 --ninitops=20000000 --nkeys=20000000 --threads=${THREADS}
+			timeout 30  ./mttest ${WORKLOAD} --nops1=1000000 --ninitops=20000000 --nkeys=20000000 --threads=${THREADS}
 		done
 		python get_average.py "${WORKLOAD},${THREADS}" >> ${Oname}
 	done
