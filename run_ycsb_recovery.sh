2b51b49794fbec63206f91b04aa6f5b4221f3a13
daksunt <davidteksenaksun@gmail.com>
2018-08-03 21:17:42 +0200

    full experiment

--- run_ycsb_recovery.sh
+++ run_ycsb_recovery.sh
@@ -17,9 +17,9 @@ for WORKLOAD in ycsb_a_uni_recovery ycsb_a_zipf_recovery; do
 		echo ${i}
 		rm -rf /scratch/tmp/nvm.*
 		rm -rf /dev/shm/incll/nvm.*
-		./mttest ${WORKLOAD} --nops1=10000000 --ninitops=200 --nkeys=200 -j8
+		./mttest ${WORKLOAD} --nops1=10000000 --ninitops=20000000 --nkeys=20000000 -j8
 		echo -e "\n\n\n\n"
-		${use_gdb} ./mttest ${WORKLOAD} --nops1=10000000 --ninitops=200 --nkeys=200 -j8
+		${use_gdb} ./mttest ${WORKLOAD} --nops1=10000000 --ninitops=20000000 --nkeys=20000000 -j8
 		sleep 1
 	done
 	python get_recovery.py "${WORKLOAD}" >> ${Oname}
