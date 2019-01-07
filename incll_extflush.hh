c476d87e411fcb73ed54080fea4eff69d215f176
daksunt <davidteksenaksun@gmail.com>
2019-01-07 11:37:35 +0100

    update licensing

--- incll_extflush.hh
+++ incll_extflush.hh
@@ -1,10 +1,17 @@
-//
-//  fences.hpp
-//  NVMlog
-//
-//  Created by Nachshon Cohen on 02.01.17.
-//  Copyright © 2017 Nachshon Cohen. All rights reserved.
-//
+/* Modification of Masstree
+ * VLSC Laboratory
+ * Copyright (c) 2018-2019 Ecole Polytechnique Federale de Lausanne
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Masstree LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Masstree LICENSE file; the license in that file
+ * is legally binding.
+ */
 
 #pragma once
 
